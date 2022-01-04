37a7bc8dfd102c7109c14511f046c426b35b6b0c
kshitijsharma <52919574+kshitijsharma@users.noreply.github.com>
2022-01-04 14:46:56 +0530

    Update MainActivity.java

--- Alarm/MainActivity.java
+++ Alarm/MainActivity.java
@@ -1,17 +1,30 @@
-package com.example.sms;
-
-import android.app.TimePickerDialog; import android.media.Ringtone; import android.media.RingtoneManager; import android.os.Build;
-import   android.os.Bundle; import android.os.Handler; import   android.os.Looper; import android.provider.Settings; import android.util.Log;
-import   android.view.View; import android.widget.Button; import android.widget.TextClock; import android.widget.TextView; import android.widget.TimePicker;
+package com.example.ex09_alarmclock;
+import android.app.TimePickerDialog;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity; import java.util.Calendar;
-import java.util.Timer; import java.util.TimerTask;
-import java.util.concurrent.atomic.AtomicInteger; public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
+public class MainActivity extends AppCompatActivity {
     TimePicker alarmtime;
     TextClock currtime;
     TextView t;
-    Button setAlarm,cancel,snz;
+    Button setAlarm,cancel,snz,ok;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,84 +35,94 @@ import java.util.concurrent.atomic.AtomicInteger; public class MainActivity exte
         cancel = findViewById(R.id.cancel);
         t = findViewById(R.id.text);
         snz= findViewById(R.id.snooze);
-        final AtomicInteger value = new AtomicInteger(0);
+        ok=findViewById(R.id.ok);
+
         final AtomicInteger stop = new AtomicInteger(0);
         final Timer timer = new Timer();
-        final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(),RingtoneManager.getDefault Uri(RingtoneManager.TYPE_ALARM));
+        final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(),RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
+        //final Ringtone r1 = RingtoneManager.getRingtone(getApplicationContext(),RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
         setAlarm.setOnClickListener(new View.OnClickListener() {
- 	@Override
+            @Override
+            public void onClick(View view) {
+                alarmtime.setVisibility(View.VISIBLE);
+                ok.setVisibility(View.VISIBLE);
+                stop.getAndSet(0);
+                timer.scheduleAtFixedRate(new TimerTask() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    @Override
+                    public void run() {
 
+                        Log.i("here1",currtime.getText().toString());
+                        Log.i("here2",AlarmTime());
+                        if(currtime.getText().toString().equals(AlarmTime())&&stop.get()==0){
+                            Log.i("here3","Works");
+                            r.play();
 
-            alarmtime.setVisibility(View.VISIBLE);
- 	stop.getAndSet(0);
- 	timer.scheduleAtFixedRate(new TimerTask() {
-                @RequiresApi(api = Build.VERSION_CODES.M)
-                @Override
-                public void run() {
-
-                    Log.i("here1",currtime.getText().toString());
-                    Log.i("here2",AlarmTime());
-
-                    if(currtime.getText().toString().equals(AlarmTime())&&stop.get()==0){
-                        Log.i("here3","Works"); r.play();
-                        runOnUiThread(new Runnable() { @Override
-                        public void run() { t.setText("ALARM IS RINGING");
-                            snz.setVisibility(View.VISIBLE);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    t.setText("ALARM IS RINGING");
+                                    snz.setVisibility(View.VISIBLE);
+                                }
+                            });
                         }
-                        });
-                    }
-                    else{
-                        runOnUiThread(new Runnable() { @Override
-                        public void run() { t.setText("");
-                            snz.setVisibility(View.INVISIBLE);
+                        else{
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    t.setText("");
+                                    snz.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                            r.stop();
                         }
-                        });
-                        r.stop();
                     }
-                }
-            },0,1000); //you want the delay to be zero because the task should be immediate and you want to check each second
-        }
-    });
-cancel.setOnClickListener(new View.OnClickListener() { @Override
-        public void onClick(View view) { stop.getAndSet(1);
-        }
-    });
+                },0,1000); //you want the delay to be zero because the task should be immediate and you want to check each second
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stop.getAndSet(1);
+            }
+        });
 
- 	snz.setOnClickListener(new View.OnClickListener() {
-        @RequiresApi(api = Build.VERSION_CODES.M)
-        @Override
-        public void onClick(View v) {
-            Integer alarmMin = alarmtime.getMinute();
+        snz.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                Integer alarmMin = alarmtime.getMinute();
+                alarmtime.setMinute(alarmMin+5);
 
-            alarmtime.setMinute(alarmMin+5);
 
+            }
+        });
 
-        }
-    });
-}
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alarmtime.setVisibility(View.INVISIBLE);
+                ok.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+    }
     @RequiresApi(api = Build.VERSION_CODES.M)
-    public String AlarmTime(){
+    public String AlarmTime() {
         Integer alarmHour = alarmtime.getHour();
         Integer alarmMin = alarmtime.getMinute();
 
-        String alarm_time,end="AM",min,hr;
-        if (alarmHour>=12){
+        String alarm_time, end = "AM", min, hr;
+        if (alarmHour >= 12) {
             end = "PM";
-            if (alarmHour!=12)
-                alarmHour-=12;
+            if (alarmHour != 12)
+                alarmHour -= 12;
         }
-        if (alarmMin<10)
-            min = "0"+String.valueOf(alarmMin);
-        else{
-            min = String.valueOf(alarmMin);
-        }
-        if (alarmHour==0)
-            hr="12";
-        else{
-            hr = String.valueOf(alarmHour);
-        }
-        alarm_time = hr+":"+min+" "+end;
+
+        hr = String.valueOf(alarmHour);
+        min = String.valueOf(alarmMin);
+        alarm_time = hr + ":" + min + " " + end;
         return alarm_time;
     }
 }
-
